1148be5fcb8818ea53263f883a547e3561bad252
Sanved <diwaresanved@gmail.com>
2025-09-23 07:56:59 +0530

    Syntax Error 1

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:4000",
-  process.env.FRONTEND_URL1.
+  process.env.FRONTEND_URL1,
   process.env.FRONTEND_URL2 // optional: from .env for deployment
 ];
 
